b680a4ae25a394c54eae58298e22af38956e07fc
otsyb <otsyb@softserveinc.com>
2021-09-28 10:47:25 +0300

    Finished part 2

--- concurrency/2/main.go
+++ concurrency/2/main.go
@@ -5,7 +5,7 @@
 // service will kill your process, unless you are a paid premium user.
 //
 // Beginner Level: 10s max per request
-// Advanced Level: 10s max per user (accumulated) 
+// Advanced Level: 10s max per user (accumulated)
 // Hint: time.Ticker can be used
 // Hint 2: to calculate timediff for Advanced lvl use:
 //
@@ -14,8 +14,12 @@
 //	t := time.Now()
 //	elapsed := t.Sub(start) // 1s or whatever time has passed
 
-
 package main
+
+import (
+	"context"
+	"time"
+)
 
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
@@ -28,8 +32,27 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	res := true
+	ch := make(chan bool)
+	var cancel context.CancelFunc
+	ctx := context.Background()
+	if !u.IsPremium {
+		ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+	}
+
+	go func() {
+		process()
+		ch <- true
+	}()
+
+	select {
+	case <-ctx.Done():
+		res = false
+	case <-ch:
+	}
+
+	return res
 }
 
 func main() {
